f2403a292cb9f3292c26c4a8707d81c68afd4abd
leonardoSuarezCortes <42702139+leonardoSuarezCortes@users.noreply.github.com>
2018-08-27 15:09:19 -0500

    Add files via upload

---
+++ prueba1.java
@@ -0,0 +1,26 @@
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class prueba1 {
+
+	@Test
+	void test() throws NumberFormatException, IOException {
+		
+
+
+}
+	 @Test
+	  public void testCalculaSalarioBruto2() {
+	    int resultadoReal = TableroJuego.movimientos;
+	  
+	    assertEquals(0, resultadoReal, 0.01);
+	  }
+}
